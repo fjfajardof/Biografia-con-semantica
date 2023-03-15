e29c01457935afb93161934672d31e2a69588a8a
Fabian Fajardo <fabianjff97@gmail.com>
2023-03-14 23:26:33 -0500

    v1

---
+++ programa.js
@@ -0,0 +1,10 @@
+function mostrarMensaje(){
+    console.log('Hicieron clink en el botón' + new Date().toLocaleTimeString())
+}
+
+
+//Agregar un eventListener a otroBoton
+
+let boton=document.getElementById("otroBoton");
+
+boton.addEventListener("click", ()=>{console.log('Hicieron clink en el botón' + new Date().toLocaleTimeString())});
